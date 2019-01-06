e086b6c61184bfbb6d213a63c8779a7da56b3089
Golden Eagle <mahyar.esteki@gmail.com>
2019-01-07 02:48:25 +0330

    Provide Create and Delete for Role

--- controllers/UserManagement.py
+++ controllers/UserManagement.py
@@ -1,10 +1,10 @@
 import sys
-
 import random, json
 from flask import *
 from flask_cors import *
 import App
 from models.DatabaseContext import *
+from datetime import datetime
 
 @App.app.route('/UserManagement/Roles')
 def role_page():
@@ -14,3 +14,41 @@ def role_page():
             return render_template('UserManagement/roles.html', entries = roles)
     else:
         return redirect("/", code=302)
+
+@App.app.route('/UserManagement/CreateRole', methods=['GET', 'POST'])
+@cross_origin(supports_credentials=True)
+def CreateRole():
+    if session.get("user_id") is not None and session.get("fullname") is not None:
+        with db_session:
+            data = request.get_json()
+            Roles(RoleTitle = data['RoleTitle'], Description = data['Description'], LatestUpdateDate = datetime.now())
+            message = "Success"
+            return jsonify({'message': message})
+    else:
+        return redirect("/", code=302)
+
+@App.app.route('/UserManagement/GetRole', methods=['GET', 'POST'])
+@cross_origin(supports_credentials=True)
+def GetRole():
+    if session.get("user_id") is not None and session.get("fullname") is not None:
+        with db_session:
+            data = request.get_json()
+            query= Roles.select(lambda u: u.RoleID == int(data['RoleID']))
+            mylist = list(query)
+            return jsonify({'RoleID': mylist[0].RoleID, 'RoleTitle': mylist[0].RoleTitle, 'Description': mylist[0].Description})
+    else:
+        return redirect("/", code=302)
+
+
+@App.app.route('/UserManagement/DeleteRole', methods=['GET', 'POST'])
+@cross_origin(supports_credentials=True)
+def DeleteRole():
+    if session.get("user_id") is not None and session.get("fullname") is not None:
+        with db_session:
+            data = request.get_json()
+            delete(p for p in Roles if p.RoleID == int(data["RoleID"]))
+            message = "Success"
+            return jsonify({'message': message})
+    else:
+        return redirect("/", code=302)
+            
---
+++ static/js/roles.js
@@ -0,0 +1,137 @@
+var result = {};
+
+function CreateRole(roleTitle, description)
+{
+    $.ajaxSetup({
+        type: "POST",
+        data: {},
+        dataType: 'json',
+        xhrFields: {
+            withCredentials: true
+        },
+        crossDomain: true,
+        contentType: 'application/json; charset=utf-8'
+    });
+
+    var jsondata =	{
+        "RoleTitle": roleTitle,
+        "Description": description
+    };
+    var a = JSON.stringify(jsondata);
+
+    $.ajax({
+        url: '/UserManagement/CreateRole',
+        type: 'POST',
+        dataType: 'json',
+        crossDomain: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            "Accept" : "application/json"
+        },
+        contentType: "application/json; charset=utf-8",
+        data: a,
+        success: function (data) {
+            if (data['message'] == "Success") {
+                $('#CreateEditModal').hide();
+                $('#CreateEditModal #RoleTitle').val('');
+                $('#CreateEditModal #Description').val('');
+                $('#CreateEditModal #selectedID').val('');
+                location.reload();
+            }
+            else
+            {
+                $("#Error").text(data['message']);
+            }
+        },
+        error: function (data, xmlHttpRequest, errorText, thrownError) {
+            alert(errorText);
+        }
+    });
+}
+
+function GetRole(roleID)
+{
+    $.ajaxSetup({
+        type: "POST",
+        data: {},
+        dataType: 'json',
+        xhrFields: {
+            withCredentials: true
+        },
+        crossDomain: true,
+        contentType: 'application/json; charset=utf-8'
+    });
+
+    var jsondata =	{
+        "RoleID": roleID
+    };
+    var a = JSON.stringify(jsondata);
+
+    $.ajax({
+        url: '/UserManagement/GetRole',
+        type: 'POST',
+        dataType: 'json',
+        crossDomain: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            "Accept" : "application/json"
+        },
+        contentType: "application/json; charset=utf-8",
+        data: a,
+        success: function (data) {
+            result = data;
+        },
+        error: function (data, xmlHttpRequest, errorText, thrownError) {
+            result = errorText;
+        }
+    });
+}
+
+
+function DeleteRole(roleID)
+{
+    $.ajaxSetup({
+        type: "POST",
+        data: {},
+        dataType: 'json',
+        xhrFields: {
+            withCredentials: true
+        },
+        crossDomain: true,
+        contentType: 'application/json; charset=utf-8'
+    });
+
+    var jsondata =	{
+        "RoleID": roleID
+    };
+    var a = JSON.stringify(jsondata);
+
+    $.ajax({
+        url: '/UserManagement/DeleteRole',
+        type: 'POST',
+        dataType: 'json',
+        crossDomain: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            "Accept" : "application/json"
+        },
+        contentType: "application/json; charset=utf-8",
+        data: a,
+        success: function (data) {
+            if (data['message'] == "Success") {
+                $('#DeleteModal').hide();
+                $('#DeleteModal #RoleTitle').val('');
+                $('#DeleteModal #Description').val('');
+                $('#DeleteModal #selectedID').val('');
+                location.reload();
+            }
+            else
+            {
+                $("#Error").text(data['message']);
+            }
+        },
+        error: function (data, xmlHttpRequest, errorText, thrownError) {
+            alert(errorText);
+        }
+    });
+}
